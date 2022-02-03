ce7859c7cf5d7da6da27f42ec646172bc0834e77
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-03 22:53:57 +0000

    commit

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -6,12 +6,40 @@
 # **************************
 # Don't change or move
 Contact.destroy_all
+Company.destroy_all
+
 # **************************
 
 # 1a. check out the schema file
 # 1b. check out the model file
 
 # 2. create 1-2 new contacts for each company (they can be made up)
+
+values = { name: "Apple Inc.", url: "https://www.apple.com", city: "Cupertino", state: "CA" }
+apple = Company.new(values)
+apple.save
+
+values = { name: "Amazon.com, Inc.", url: "https://www.amazon.com", city: "Seattle", state: "WA" }
+amazon = Company.new(values)
+amazon.save
+
+apple = Company.where({ name: "Apple Inc." })[0]
+apple_id = apple.read_attribute(:id)
+
+amazon = Company.where({ name: "Amazon.com, Inc." })[0]
+amazon_id = amazon.read_attribute(:id)
+
+values = { first_name: "Tim", last_name: "Cook", email: "tim@apple.com", phone_number: "777-777-7777", company_id: apple_id }
+contact = Contact.new(values)
+contact.save
+
+values = { first_name: "Craig", last_name: "Federighi", email: "craig@apple.com", phone_number: "888-888-8888", company_id: apple_id }
+contact = Contact.new(values)
+contact.save
+
+values = { first_name: "Andy", last_name: "Jassy", email: "andy@amazon.com", phone_number: "666-666-6666", company_id: amazon_id }
+contact = Contact.new(values)
+contact.save
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
@@ -21,3 +49,9 @@ Contact.destroy_all
 # Craig Federighi - craig@apple.com
 # Elon Musk - elon@tesla.com
 # Tim Cook - tim@apple.com
+
+puts "Contacts: #{Contact.all.count}"
+contacts = Contact.all
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+end 
