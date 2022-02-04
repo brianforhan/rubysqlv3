198b20e63c1356570351732cc05a68cb0cd1d2cf
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-04 14:56:38 +0000

    commit

--- code-along-complete/2-associations.rb
+++ code-along-complete/2-associations.rb
@@ -18,9 +18,9 @@ apple = Company.where({ name: "Apple Inc." })[0]
 apple_id = apple.id
 contacts = Contact.where(company_id: apple_id)
 
-p "Contacts at Apple: #{contacts.count}"
+puts "Contacts at Apple: #{contacts.count}"
 for contact in contacts
-    p "#{contact.first_name} #{contact.last_name}"
+    puts "#{contact.first_name} #{contact.last_name}"
 end
 
 # BEST
