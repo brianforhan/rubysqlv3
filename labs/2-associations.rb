de08232a3227fbf5baf8257d908ee528d173e2fb
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-04 01:25:48 +0000

    committ

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -5,8 +5,6 @@
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
 
-Contact.destroy_all
-Company.destroy_all
 
 # ---------------------------------
 # Contacts: 4
@@ -15,49 +13,21 @@ Company.destroy_all
 # Elon Musk - elon@tesla.com - Tesla, Inc.
 # Tim Cook - tim@apple.com - Apple Inc.
 
-values = { name: "Apple Inc.", url: "https://www.apple.com", city: "Cupertino", state: "CA" }
-apple = Company.new(values)
-apple.save
-
-values = { name: "Amazon Inc.", url: "https://www.amazon.com", city: "Seattle", state: "WA" }
-amazon = Company.new(values)
-amazon.save
-
-apple = Company.where({ name: "Apple Inc." })[0]
-apple_id = apple.read_attribute(:id)
-
-amazon = Company.where({ name: "Amazon Inc." })[0]
-amazon_id = amazon.read_attribute(:id)
-
-values = { first_name: "Tim", last_name: "Cook", email: "tim@apple.com", phone_number: "777-777-7777", company_id: apple_id }
-contact = Contact.new(values)
-contact.save
-
-values = { first_name: "Craig", last_name: "Federighi", email: "craig@apple.com", phone_number: "888-888-8888", company_id: apple_id }
-contact = Contact.new(values)
-contact.save
-
-values = { first_name: "Andy", last_name: "Jassy", email: "andy@amazon.com", phone_number: "666-666-6666", company_id: amazon_id }
-contact = Contact.new(values)
-contact.save
-
 puts "Contacts: #{Contact.all.count}"
 contacts = Contact.all
-company = Company.where(id: contact.company_id)[0]
 for contact in contacts
+    company = Company.where(id: contact.company_id)[0]
     puts "#{contact.first_name} #{contact.last_name} - #{contact.email} #{company.name}"
-end 
+end
 
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
 
+puts "Contacts: #{Contact.all.count}"
 contacts = Contact.all
-company = Company.where(id: contact.company_id)[0]
 for contact in contacts
-    puts "#{company.name}
-    #{contact.first_name} #{contact.last_name}
-    #{contact.email}
-    "
-end 
+    company = Company.where(id: contact.company_id)[0]
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} #{company.name}"
+end
 
 # ---------------------------------
 # Apple Inc.
